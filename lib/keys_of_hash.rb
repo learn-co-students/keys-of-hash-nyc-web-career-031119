5181aec43d681bce5cd53aad0aadf9b1abeaa1a5
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-07 03:29:33 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,41 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+      if arguments.length == 1
+        argument_string = arguments.join
+        n = 0
+         
+        if arguments[0].class == Fixnum
+          argument_string = arguments[0]
+          
+        end
+        
+        while n < keys.length
+        
+          if values[n] == argument_string
+            
+            
+            array << keys[n]
+            
+          end
+          n+=1
+        end
+      else
+        n = 0
+        while arguments.length > n
+          
+          argument_string = arguments[n]
+          i = 0
+          while i < keys.length
+            if values[i] == argument_string
+              array << keys[i]
+            end
+            i+=1
+          end
+          n+=1
+        end
+      end
+    return array
   end
 end
